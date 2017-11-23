5324c05ecdba4a09da903b7f19a8a5de8a99a68f
mikaelhellqvist <mikaelhellqvist@gmail.com>
2017-11-23 16:49:31 +0100

    Initial Commit

---
+++ GreatCircle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GreatCircle
+//
+//  Created by Mikael Hellqvist on 2017-11-23.
+//  Copyright © 2017 Mikael Hellqvist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
